d5e06764c0c4e4943624e0f9fa76bd101b87e0e6
kamal batesha <bateshakamal@gmail.com>
2025-07-29 23:47:49 +0300

    merge dash-board-both

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import SignUp from './pages/SignUp'
 import UserContextProvider from './context/UserContext'
 import FormInput from './components/formInput/FormInput'
 import FormRole from './components/formRole/FormRole'
+import { AdminDashBoard } from './pages/Admin/AdminDashBoard'
+import AdminLayout from './components/AdminLayout/AdminLayout'
+import {ProviderDashBoard} from './pages/Provider/ProviderDashBoard'
+import ProviderLayout from './components/ProviderLayout/ProviderLayout'
+import Providers from './pages/Admin/Providers'
+import './App.css'
+import SignUpDetails from './components/signUpDetails/SignUpDetails';
+import { Toaster } from 'react-hot-toast'
+import Login from './pages/Login'
+import AcountStatus from './pages/AcountStatus'
+import Pending from './components/Pending';
+import Rejected from './components/Rejected'
+import Confirmed from './components/Confirmed'
 
 function App() {
 
@@ -17,16 +30,41 @@ function App() {
       ),
     },
     {
-      path: "/signUp",
-      element: <SignUp />,
-      children: [
+      path:"/signUp",
+      element:<SignUp/>,
+      children:[
         {
           index:true,
-          element:<FormInput/>
+          element: <FormInput/>
         },
         {
           path:"role",
           element:<FormRole/>
+        },
+        {
+          path:"provider",
+          element:<SignUpDetails/>
+        }
+      ]
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    },{
+      path:"/acountStatus",
+      element:<AcountStatus/>,
+      children:[
+        {
+          path:"pending",
+          element:<Pending/>
+        },
+        {
+          path:"rejected",
+          element:<Rejected/>
+        },
+        {
+          path:"confirm/:token",
+          element:<Confirmed/>
         }
       ]
     },
