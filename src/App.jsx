8e879469e7b03a99aff758c924c158e543dce4c1
kamal batesha <bateshakamal@gmail.com>
2025-07-30 00:12:23 +0300

    merge profilePart

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import SignUp from './pages/SignUp'
 import UserContextProvider from './context/UserContext'
 import FormInput from './components/formInput/FormInput'
-import FormRole from './components/formRole/FormRole'
-import FormRole from './components/formRole/FormRole'
 import { AdminDashBoard } from './pages/Admin/AdminDashBoard'
 import AdminLayout from './components/AdminLayout/AdminLayout'
 import {ProviderDashBoard} from './pages/Provider/ProviderDashBoard'
@@ -18,8 +16,8 @@ import AcountStatus from './pages/AcountStatus'
 import Pending from './components/Pending';
 import Rejected from './components/Rejected'
 import Confirmed from './components/Confirmed'
-import AdminLayout from "./components/layout/AdminLayout";
 import Profile from "./pages/Profile";
+import FormRole from './components/formRole/FormRole';
 
 function App() {
 
