4072d99ac44d7c572dccc9f31c0a32373c6e4eb0
Ashrakat A Elkholy <ashrakatalkholy@gmail.com>
2025-07-30 22:24:52 +0300

    updated av and sidebar

--- src/components/AdminLayout/AdminLayout.jsx
+++ src/components/AdminLayout/AdminLayout.jsx
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import { TopNav } from './TopNav';
-import { AdminSidebar } from './SideBar';
+import { Sidebar } from './SideBar';
 
 const AdminLayout = ({ children }) => {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
+
   const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
 
   return (
     <div className="flex h-screen overflow-hidden bg-[#FBFCFE]" dir="rtl">
       {/* Sidebar on the right */}
-      <AdminSidebar isOpen={isSidebarOpen} />
-      
+      <Sidebar isOpen={isSidebarOpen} />
+
       {/* Main content area */}
-      <div className={`flex-1 flex flex-col transition-all duration-300 ${isSidebarOpen ? 'mr-64' : 'mr-0'}`}>
-        <TopNav 
-          sidebarOpen={isSidebarOpen} 
-          setSidebarOpen={setSidebarOpen}
-        />
+      <div className={`flex-1 flex flex-col transition-all duration-300 ${isSidebarOpen ? 'pr-45' : 'pr-0'}`}>
+        <TopNav toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
         <main className="flex-1 overflow-y-auto p-6">
           {children}
         </main>
--- src/components/ProviderLayout/SideBar.jsx
+++ src/components/ProviderLayout/SideBar.jsx
@@ -7,10 +7,9 @@ export const Sidebar = ({ isOpen }) => {
 
     const mainMenu = [
         { id: 'dashboard', icon: 'home-filled.png', activeIcon: 'home.png', label: 'الرئيسية', path: '/' },
-        { id: 'applications', icon: 'menu-board-filled.png', activeIcon: 'menu-board.png', label: 'طلباتى' },  
-        // , path: '/orders'
-        { id: 'reports', icon: 'category-filled.png', activeIcon: 'category.png', label: 'خدماتى', path: '/my-services' },
-        { id: 'students', icon: 'message-filled.png', activeIcon: 'message-outline.png', label: 'الرسائل', path: '/Messages' },
+        { id: 'applications', icon: 'menu-board-filled.png', activeIcon: 'menu-board.png', label: 'طلباتى', path: '/orders' },
+        { id: 'reports', icon: 'category-filled.png', activeIcon: 'category.png', label: 'خدماتى', path: '/services' },
+        { id: 'students', icon: 'message-filled.png', activeIcon: 'message-outline.png', label: 'الرسائل', path: '/clients' },
         { id: 'reports', icon: 'profile-filled.png', activeIcon: 'profile.png', label: 'الاعدادات', path: '/profile' },
     ];
 
